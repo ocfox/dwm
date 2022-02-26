3f3782126a13cedf76caff4d920581076c10b63d
ocfox <i@ocfox.me>
2022-02-26 21:54:10 +0800

    flake: fix libXext

--- flake.nix
+++ flake.nix
@@ -1,27 +1,39 @@
 {
-  description = "Customized dwm";
+  description = "A very basic flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-
-      overlay = final: prev: {
-        dwm = prev.dwm.overrideAttrs (old: {
-          buildInputs = (old.buildInputs or []) ++ [ final.xorg.libXext ];
-          src = builtins.path { path = ./src; name = "dwm"; };
-        });
-      };
-    in
-    {
-      inherit overlay;
-
-      checks.${system}.build = (
-        import nixpkgs {
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
           inherit system;
-          overlays = [ overlay ];
-        }
-      ).dwm;
-    };
+          overlays = [
+            (final: prev: {
+              dwm = prev.dwm.overrideAttrs (oldAttrs: rec {
+                version = "master";
+                src = ./src;
+              });
+            })
+          ];
+        };
+      in
+      rec {
+        apps = {
+          dwm = {
+            type = "app";
+          };
+        };
+
+        packages.dwm = pkgs.dwm;
+        defaultApp = apps.dwm;
+        defaultPackage = pkgs.dwm;
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ xorg.libX11 xorg.libXft xorg.libXinerama gcc bear xorg.libXext ];
+        };
+      }
+    );
 }
