cc80bb21b1f7be74d0ac550989f091c2f4b85f23
ocfox <i@ocfox.me>
2022-02-26 21:33:49 +0800

    feat: flake support

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Customized dwm";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+
+      overlay = final: prev: {
+        dwm = prev.dwm.overrideAttrs (old: {
+          src = builtins.path { path = ./src; name = "dwm"; };
+        });
+      };
+    in
+    {
+      inherit overlay;
+
+      checks.${system}.build = (
+        import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
+        }
+      ).dwm;
+    };
+}
