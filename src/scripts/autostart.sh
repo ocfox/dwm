9e26984937b96999c91f1371cd740f00776ac569
ocfox <i@ocfox.me>
2022-02-26 21:25:59 +0800

    scripts: remove battery on dwmbar

--- src/scripts/autostart.sh
+++ src/scripts/autostart.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 # Uncomment this line to use those feature
-# picom -b
+picom -b
+feh --bg-fill ~/pictures/cirno-a.png
 # feh --randomize --recursive --bg-fill ~/Pictures/Wallpapers/
 $HOME/.local/share/dwm/dwm-bar &
